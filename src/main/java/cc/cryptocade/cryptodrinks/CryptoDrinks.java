62eba0cdb6d554056421ba9f48d09eb3dd2bcbe6
kscottdev <kscottdev@gmail.com>
2020-08-25 23:43:12 -0400

    Initial commit

---
+++ src/main/java/cc/cryptocade/cryptodrinks/CryptoDrinks.java
@@ -0,0 +1,17 @@
+package cc.cryptocade.cryptodrinks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CryptoDrinks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
